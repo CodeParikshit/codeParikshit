7f27a6f34c7c29484ec1b33a90d8ddbdc3f01350
CodeParikshit <146836129+CodeParikshit@users.noreply.github.com>
2023-10-03 04:44:48 -0700

    Add files via upload

---
+++ integer2.java
@@ -0,0 +1,13 @@
+class integer2{
+public static void main(String args[])
+{
+byte b = 2;
+int x = 20;
+short s = 200;
+long l = 20000;
+System.out.println("b = "+b);
+System.out.println("x = "+x);
+System.out.println("s = "+s);
+System.out.println("l = "+l);
+}
+}
