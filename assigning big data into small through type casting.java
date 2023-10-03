807c40c29be7fbb633136abc8e3be82666ce8ec6
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-03 04:54:59 -0700

    Add files via upload

---
+++ assigning big data into small through type casting.java
@@ -0,0 +1,11 @@
+class problem1{
+public static void main(String args[])
+{
+int a = 20;
+byte b ;
+b = (byte)a;
+// we can not insert big datatype into small datatype.
+System.out.println("a = "+a);
+System.out.println("b = "+b);
+}
+}
