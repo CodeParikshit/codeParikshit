c6db1dc52b36c7ef408aa0c993e6136926007376
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-03 04:59:11 -0700

    Add files via upload

---
+++ FloatFunction.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class FloatFunction{
+public static void main(String args[])
+{
+Scanner sc = new Scanner (System.in);
+System.out.println("enter the value for float =");
+float f;
+f = sc.nextFloat();
+System.out.println("the value of float is ="+f);
+}
+}
