bdf264960b4265bbe9882fe9495ac53f69d3d5bb
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-03 04:58:52 -0700

    Add files via upload

---
+++ DoubleFunction.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class DoubleFunction{
+public static void main(String args[])
+{
+Scanner sc = new Scanner (System.in);
+System.out.println("enter the value for double  =");
+double d;
+d = sc.nextDouble();
+System.out.println("the value of double is ="+d);
+}
+}
