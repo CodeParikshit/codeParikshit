ca4a88d2674e2a3dff4906df39afe942c9dfec17
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-03 04:55:20 -0700

    Add files via upload

---
+++ assigning value to float.java
@@ -0,0 +1,8 @@
+class flirtingwithfloatsol{
+public static void main(String args[])
+{
+float f =5.5F;//by default it is a doible no. so weconverted into float....
+
+System.out.println("f = "+f);
+}
+}
