8fc35160d599ba831b2840f686abd86feb84392d
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-03 04:56:23 -0700

    Add files via upload

---
+++ we can't inserrt big data into small data.java
@@ -0,0 +1,10 @@
+class largeintosmall{
+public static void main(String args[])
+{
+int a = 10;
+byte b = a;
+// we can not insert big datatype into small datatype.
+System.out.println("a = "+a);
+System.out.println("b = "+b);
+}
+}
