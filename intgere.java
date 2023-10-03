c640ce6b9649a24363b44ab948f99e493aee9941
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-03 04:59:28 -0700

    Add files via upload

---
+++ intgere.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class intgere{
+public static void main(String args[])
+{
+Scanner sd = new Scanner(System.in);
+System.out.println("enter value for short =");
+short v;
+v = sd.nextShort();
+}
+}
