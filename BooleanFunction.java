cf57ea8e75d372a03c25ac5e3b1e95f5885fd235
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-03 04:58:02 -0700

    Add files via upload

---
+++ BooleanFunction.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class BooleanFunction{
+public static void main(String args[])
+{
+Scanner sc = new Scanner (System.in);
+System.out.println("enter the boolean =");
+boolean b;
+b = sc.nextBoolean();
+System.out.println("the boolean is  ="+b);
+}
+}
