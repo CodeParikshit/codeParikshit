541f799b3b2842fb40b82ce97bf971e54a7d81f3
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-03 04:58:36 -0700

    Add files via upload

---
+++ CharacterFunction.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class CharacterFunction{
+public static void main(String args[])
+{
+Scanner sc = new Scanner (System.in);
+System.out.println("enter the character =");
+Character c;
+c = sc.next().charAt(0);
+System.out.println("the Character is  ="+c);
+}
+}
