b0028559068b3cf3d950cf750ac81d3c80821ad8
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-03 04:58:19 -0700

    Add files via upload

---
+++ Bytefun.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Bytefun{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("enter the value :- ");
+byte x;
+x =sc.nextByte();
+System.out.println("so the value of byte is -"+x);
+}
+}
