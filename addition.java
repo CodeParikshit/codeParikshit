e0f28195bdf798a31afd158bda8de49f22c24257
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-03 05:00:12 -0700

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,14 @@
+import java.util.*;  
+class sequence
+{  
+public static void main(String[] args)  
+{  
+int sum,n1,n2;
+Scanner sc= new Scanner(System.in); 
+System.out.println("Enter value : ");
+n1 = sc.nextInt();
+n2 = sc.nextInt();
+sum = n1+n2;
+System.out.println("sum = "+sum);
+}  
+}  
