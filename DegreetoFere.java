ea6be1e6780cbfb4b92529407016571dda9c8170
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-04 09:04:34 -0700

    Add files via upload

---
+++ DegreetoFere.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class DegreetoFere{
+public static void main(String args[])
+{
+double celcius,result;
+Scanner sc = new Scanner (System.in);
+System.out.println("enter the value for degree celcius");
+celcius = sc.nextDouble();
+
+result =1.8*celcius+32;
+System.out.println("degree to ferenite = "+result);
+}
+}
+ 
