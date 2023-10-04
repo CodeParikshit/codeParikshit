39c3335af2924690bce43e5d42bea3ab107d1bbf
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-04 09:07:03 -0700

    Add files via upload

---
+++ Squareroot.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Squareroot{
+public static void main(String args[])
+{
+double num;
+Scanner sc = new Scanner (System.in);
+System.out.println("enter any num");
+num= sc.nextDouble();
+double result=num*num;
+System.out.println("sqrt of no. is  = "+result);
+}
+}
+ 
