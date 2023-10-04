7b5a580ebbc07577d64bbffb77734f4bd0c0f417
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-04 09:05:51 -0700

    Add files via upload

---
+++ MetertoCm.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class MetertoCm{
+public static void main(String args[])
+{
+double meter,cm;
+Scanner sc = new Scanner (System.in);
+System.out.println("enter the meter");
+meter = sc.nextDouble();
+
+cm=100*meter;
+System.out.println("conversion of meter to cm = "+cm);
+}
+}
+ 
