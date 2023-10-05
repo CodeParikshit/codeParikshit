b47c9e339092648687435bf611c5dab01f18c087
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-04 22:56:49 -0700

    Add files via upload

---
+++ forloop/Sum_of_Natural_no.java
@@ -0,0 +1,12 @@
+class Sum_of_Natural_no{
+public static void main(String args[]){
+int num=10;
+int sum=0;
+int i;
+for (i=1;i<=10;i++){
+sum +=i;
+System.out.println(i);
+}
+System.out.println("the sum of " +num+ "positive no. is " +sum);
+}
+}
