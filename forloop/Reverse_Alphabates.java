cd88956541724338c60670860d98f008b1da4d21
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-06 08:49:00 -0700

    Add files via upload

---
+++ forloop/Reverse_Alphabates.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class  Reverse_Alphabates{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+char i;
+for (i='a';i<='z';i--){
+
+System.out.print(i);
+}
+}
+}
