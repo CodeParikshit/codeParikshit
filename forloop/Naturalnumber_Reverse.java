defe24e0be9f2a8bbd00b26ddcb1c2c8f0f2dd3c
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-06 08:26:12 -0700

    Add files via upload

---
+++ forloop/Naturalnumber_Reverse.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Naturalnumber_Reverse{
+public static void main(String args[]){
+int N;
+Scanner sc = new Scanner (System.in);
+System.out.println("enter the n natural no");
+N = sc.nextInt();
+int i,j;
+System.out.println("all natural no. in reverse are");
+for (i=N;i>=1;i--){
+System.out.println(i);
+}
+System.out.println("all natural no in are");
+for (j=1;j<=N;j++){
+System.out.print(j);
+
+}
+}
+}
+
+
+
+
