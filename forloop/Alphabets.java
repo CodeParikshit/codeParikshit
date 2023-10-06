5f8bbefc890ed26c73d77a0c8039644dfa2e8031
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-06 08:44:28 -0700

    Add files via upload

---
+++ forloop/Alphabets.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Alphabets{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+char i;
+for (i='a';i<='z';i++){
+
+System.out.print(i);
+}
+}
+}
