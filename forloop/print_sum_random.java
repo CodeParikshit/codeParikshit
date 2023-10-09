34c9793a605104b29617e1756f599e9619cf08e0
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-10-09 09:50:31 -0700

    Add files via upload

---
+++ forloop/print_sum_random.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class print_sum_random{
+public static void main(String args[]){
+int c;
+int n;
+int i;
+Scanner sc = new Scanner (System.in);
+System.out.println("enter the no.");
+n = sc.nextInt();
+for (i=1;i<=n;i++){
+c=i%2;
+
+
+if(c!=0);
+System.out.println(c);
+}
+}
+}
