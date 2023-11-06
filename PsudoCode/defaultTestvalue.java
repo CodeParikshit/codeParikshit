f429eef5bd74f4020c5d8adf8e07295f58ff590d
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-11-05 21:14:23 -0800

    Add files via upload

---
+++ PsudoCode/defaultTestvalue.java
@@ -0,0 +1,12 @@
+class defaultTestvalue{
+static int x;
+static char c;
+static boolean b;
+static double d;
+public static void main(String args[]){
+System.out.println(x);
+System.out.println(c);
+System.out.println(b);
+System.out.println(d);
+}
+}
