25a47c96b3f95e339132136dd44a43db43899be6
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-11-10 21:23:58 -0800

    Add files via upload

---
+++ PsudoCode/demo01.java
@@ -0,0 +1,18 @@
+package firstexample;
+
+class demo01{
+public static void main(String args[]){
+int deno[]= {500,200,100,50,20,10,5,2,1}; //array to store denomatation.
+int rup = 3546;
+int div,i;
+for(i=0;rup>0;i++){
+if(rup/deno[i]>0)
+{
+div = rup/deno[i];
+System.out.println(deno[i]+"*"+div+"="+div*deno[i]);
+rup = rup-(div*deno[i]);
+}
+}
+}
+}
+
