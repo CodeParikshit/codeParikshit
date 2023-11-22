6c3a0cb0eb147f25851cc8393ef5adc0f3cdbe40
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-11-21 20:19:59 -0800

    Add files via upload

--- PsudoCode/xyz.java
+++ PsudoCode/xyz.java
@@ -1,24 +1,15 @@
+class xyz{
+public static void main(String args[]){
+int[] a = {10, 20 , 30 , 40};
+System.out.println(a[0]);
+System.out.println(a[1]);
+System.out.println(a[2]);
+System.out.println(a[3]);
 
-import java.io.*;
-class Dog implements Serializable
-{
-int i=10;
-int j=20;
-}
-class xyz
-{
-public static void main(String args[])throws Exception{
-Dog d1=new Dog();
-System.out.println("Serialization started");
-FileOutputStream fos=new FileOutputStream("abc.ser");
-ObjectOutputStream oos=new ObjectOutputStream(fos);
-oos.writeObject(d1);
-System.out.println("Serialization ended");
-System.out.println("Deserialization started");
-FileInputStream fis=new FileInputStream("abc.ser");
-ObjectInputStream ois=new ObjectInputStream(fis);
-Dog d2=(Dog)ois.readObject();
-System.out.println("Deserialization ended");
-System.out.println(d2.i+"................"+d2.j);
+// second approach
+for(int i = 0; i<a.length; i++){
+System.out.print(a[i]);
 }
 }
+}
+
