f21b9d603eafe4d05e555d9c5751f2b4dd6afdf5
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-12-03 21:20:48 -0800

    Add files via upload

---
+++ string/concat.java
@@ -0,0 +1,13 @@
+
+
+import java.util.Scanner;
+
+public class concat {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner (System.in);
+		String s1  = "Hello";
+		String s2 =  "world";
+		System.out.println(s1.concat(s2));
+	}
+
+}
