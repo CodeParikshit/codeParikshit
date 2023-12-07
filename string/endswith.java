ea9fb986b90732401a29e13b3f3d699d3fe1063a
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-12-06 21:11:39 -0800

    Add files via upload

---
+++ string/endswith.java
@@ -0,0 +1,12 @@
+
+
+import java.util.Scanner;
+
+public class endswith {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner (System.in);
+		String s1  = "Hello, my name is khan and i am not a terrorist";
+		System.out.println(s1.endsWith("terrorist")); //if it ends with the the 
+	}
+
+}
