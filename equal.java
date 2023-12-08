15f646c9360d873e21d0f5478b67e9465c1941e4
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-12-07 21:54:35 -0800

    Add files via upload

---
+++ equal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class equal {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner (System.in);
+		String s1  = "hello";
+		String s2 =  "hello";
+		System.out.println(s1.equals(s2));
+	}
+
+}
