94066b5305021d81388bec6eb67329b4db9e0e86
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-12-07 21:55:57 -0800

    Add files via upload

---
+++ string/equalsIgnoreCase.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class equalsIgnoreCase {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner (System.in);
+		String s1  = "Hello";
+		String s2 =  "hello";
+		System.out.println(s1.equalsIgnoreCase(s2));
+	}
+
+}
