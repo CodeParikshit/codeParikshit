bd4528050811b7ce1eb761d9c107b21e270f9bc4
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-12-12 06:10:06 -0800

    Add files via upload

---
+++ string/Space_After_each_alphabate.java
@@ -0,0 +1,9 @@
+
+public class Space_After_each_alphabate {
+	public static void main(String args[]) {
+		String s ="hi how are you";
+		for(int i=0;i<s.length();i++) {
+			System.out.print(s.charAt(i)+" ");
+		}
+	}
+}
