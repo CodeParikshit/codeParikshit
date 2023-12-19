d981739dea04332d7f478afc0a89225594fe8280
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-12-18 23:50:21 -0800

    Add files via upload

---
+++ testcarData.java
@@ -0,0 +1,17 @@
+class testcarData {
+	public static void main(String[] args) {
+		BMWModel bm1 = new BMWModel();
+		System.out.println(bm1.sh_id);
+		System.out.println(bm1.sh_name);
+		System.out.println(bm1.sh_id);
+		bm1.info();
+		System.out.println(bm1.car_id);
+		System.out.println(bm1.car_name);
+		bm1.display();
+		System.out.println(bm1.model);
+		bm1.variants();
+		
+		
+	}
+
+}
