06c536bdd966746b7c341c374fc986f0739dc9db
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-12-21 20:59:32 -0800

    Add files via upload

---
+++ oops/static_variable_accessing.java
@@ -0,0 +1,7 @@
+public class static_variable_accessing {
+	int show_id = 11321;
+	static String show_name = "fusion showroom";
+	public void info() {
+		System.out.println("i am info method");
+	}
+}
