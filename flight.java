51e4bd3a4ff523ecc0c6bc202516e8a5f0d854e2
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2023-12-25 21:32:09 -0800

    Add files via upload

---
+++ flight.java
@@ -0,0 +1,11 @@
+
+public class flight {
+	String company = "indigo";
+	String name = "bagadora airport";
+	int flight_id = 22345;
+	public void display() {
+		System.out.println(name);
+		System.out.println(flight_id);
+	}
+
+}
