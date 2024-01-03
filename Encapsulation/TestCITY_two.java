52b4c4475287ca3ee5b819c4dc3b5c71c8cc3047
Parikshit arekar <146836129+CodeParikshit@users.noreply.github.com>
2024-01-02 20:09:21 -0800

    Add files via upload

---
+++ Encapsulation/TestCITY_two.java
@@ -0,0 +1,8 @@
+ class  TestCITY_two {
+	public static void main(String[] args) {
+		description d1 = new description("red panda ","gantok");
+		city_two d2= new city_two("pune","maharashtra",d1);
+		d2.display();
+	}
+
+}
